7c0c354d6e166233caee1e02766d3d648e977fed
gdchentz <35604981@qq.com>
2023-04-19 14:01:55 +0800

    delete the test text line

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class Application {
 
-// test ok 88889996668898989
+// delete on line code
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
