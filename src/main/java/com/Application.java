5f99cdbcf9bbed68e52d2a675c095a070838f691
gdchentz <35604981@qq.com>
2023-04-19 14:00:45 +0800

    test1

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -1,17 +1,13 @@
-package com;
+package cman;
 
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
-@MapperScan("com.dao")
 public class Application {
 
-
+// test ok 88889996668898989
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
