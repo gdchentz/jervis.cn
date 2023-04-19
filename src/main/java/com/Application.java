5acc545da564d021a0d843751dda52098e65be87
gdchentz <35604981@qq.com>
2023-04-19 14:00:45 +0800

    test1

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class Application {
 
+// test ok 88889996668898989
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
