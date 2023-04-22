176709b742158f508978c0df4279925ea9596439
gdchentz <35604981@qq.com>
2023-04-22 21:38:31 +0800

    update time 21:38 at 23/04/22

--- src/main/java/com/controller/hello.java
+++ src/main/java/com/controller/hello.java
@@ -9,7 +9,7 @@ public class hello {
 
     @RequestMapping("/hello")
     public  String Hello(){
-        return  "This new string out: Ok test done! update";
+        return  "This new string out: Ok test done! update 21.37";
 
     }
 
