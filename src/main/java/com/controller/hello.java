956b1d4e6cf0e2796406e1641916511bd74702e3
gdchentz <35604981@qq.com>
2023-04-23 13:52:13 +0800

    update time 13:51 at 23/04/23

--- src/main/java/com/controller/hello.java
+++ src/main/java/com/controller/hello.java
@@ -1,16 +1,18 @@
 package com.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 
 public class hello {
 
     @RequestMapping("/hello")
-    public  String Hello(){
-        return  "This new string out: Ok test done! update 21.37";
-
+    public  String Hello(Model md){
+        md.addAttribute("mess","testkjkjfsa");
+        return "hello" ;
     }
 
 
